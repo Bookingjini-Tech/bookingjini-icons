3a1e926a3ef9f21d58a1b460ff6d74047b83d6f3
Ramanujam Gond <ramanujamgond@gmail.com>
2025-02-01 19:32:37 +0530

    feat: updated the dist directory with updated icons

--- dist/fonts/bookingjini-icons.ts
+++ dist/fonts/bookingjini-icons.ts
@@ -1,33 +1,453 @@
 export type BookingjiniIconsId =
-  | "24-hour-front-desk_dual-tone"
-  | "24-hour-front-desk_fill"
-  | "24-hour-front-desk_line"
-  | "24-Hour-Security_dual-tone"
-  | "24-Hour-Security_fill"
-  | "24-Hour-Security_line";
+  | "ac-duo-tone"
+  | "ac-fill"
+  | "ac-line"
+  | "adjoining-room-duo-tone"
+  | "adjoining-room-fill"
+  | "adjoining-room-line"
+  | "atm-duo-tone"
+  | "atm-fill"
+  | "atm-line"
+  | "baby-duo-tone"
+  | "baby-fill"
+  | "baby-line"
+  | "basket-duo-tone"
+  | "basket-fill"
+  | "basket-line"
+  | "books-duo-tone"
+  | "books-fill"
+  | "books-line"
+  | "broom-duo-tone"
+  | "broom-fill"
+  | "broom-line"
+  | "cab-duo-tone"
+  | "cab-fill"
+  | "cab-line"
+  | "call-bell-duo-tone"
+  | "call-bell-fill-1"
+  | "call-bell-fill"
+  | "cards-due-tone"
+  | "cards-duo-tone"
+  | "cards-line"
+  | "check-in-duo-tone-1"
+  | "check-in-duo-tone-2"
+  | "check-in-duo-tone-3"
+  | "check-in-duo-tone-4"
+  | "check-in-duo-tone"
+  | "check-in-fill-1"
+  | "check-in-fill-2"
+  | "check-in-fill-3"
+  | "check-in-fill-4"
+  | "check-in-fill"
+  | "check-in-line-1"
+  | "check-in-line-2"
+  | "check-in-line-3"
+  | "check-in-line-4"
+  | "check-in-line"
+  | "conference-duo-tone"
+  | "conference-fill"
+  | "conference-line"
+  | "dish-duo-tone"
+  | "dish-fill"
+  | "dish-line"
+  | "door-man-duo-tone"
+  | "door-man-fill"
+  | "door-man-line"
+  | "dry-clean-duo-tone"
+  | "dry-clean-fill"
+  | "dry-clean-line"
+  | "electric-outlet-duo-tone"
+  | "electric-outlet-fill"
+  | "electric-outlet-line"
+  | "flower-duo-tone"
+  | "flower-fill"
+  | "flower-line"
+  | "front-office-duo-tone"
+  | "front-office-fill"
+  | "front-office-line"
+  | "game-duo-tone"
+  | "game-fill"
+  | "game-line"
+  | "gym-duo-tone"
+  | "gym-fill"
+  | "gym-line"
+  | "lift-duo-tone"
+  | "lift-fill"
+  | "lift-line"
+  | "Liquor-duo-tone"
+  | "Liquor-fill"
+  | "Liquor-line"
+  | "medical-duo-tone"
+  | "medical-fill"
+  | "medical-line"
+  | "parking-duo-tone"
+  | "parking-fill"
+  | "parking-line"
+  | "pet-duo-tone"
+  | "pet-fill"
+  | "pet-line"
+  | "scissor-duo-tone"
+  | "scissor-fill"
+  | "scissor-line"
+  | "security-duo-tone"
+  | "security-fill"
+  | "security-line"
+  | "shield-check-duo-tone"
+  | "shield-check-fill"
+  | "shield-check-line"
+  | "shop-duo-tone"
+  | "shop-fill"
+  | "shop-line"
+  | "snow-flake-duo-tone"
+  | "snow-flake-fill"
+  | "snow-flake-line"
+  | "swimming-pool-duo-tone"
+  | "swimming-pool-fill"
+  | "swimming-pool-line"
+  | "track-duo-tone"
+  | "track-fill"
+  | "track-line"
+  | "tree-fence-duo-tone"
+  | "tree-fence-fill"
+  | "tree-fence-line";
 
 export type BookingjiniIconsKey =
-  | "i24HourFrontDeskDualTone"
-  | "i24HourFrontDeskFill"
-  | "i24HourFrontDeskLine"
-  | "i24HourSecurityDualTone"
-  | "i24HourSecurityFill"
-  | "i24HourSecurityLine";
+  | "AcDuoTone"
+  | "AcFill"
+  | "AcLine"
+  | "AdjoiningRoomDuoTone"
+  | "AdjoiningRoomFill"
+  | "AdjoiningRoomLine"
+  | "AtmDuoTone"
+  | "AtmFill"
+  | "AtmLine"
+  | "BabyDuoTone"
+  | "BabyFill"
+  | "BabyLine"
+  | "BasketDuoTone"
+  | "BasketFill"
+  | "BasketLine"
+  | "BooksDuoTone"
+  | "BooksFill"
+  | "BooksLine"
+  | "BroomDuoTone"
+  | "BroomFill"
+  | "BroomLine"
+  | "CabDuoTone"
+  | "CabFill"
+  | "CabLine"
+  | "CallBellDuoTone"
+  | "CallBellFill_1"
+  | "CallBellFill"
+  | "CardsDueTone"
+  | "CardsDuoTone"
+  | "CardsLine"
+  | "CheckInDuoTone_1"
+  | "CheckInDuoTone_2"
+  | "CheckInDuoTone_3"
+  | "CheckInDuoTone_4"
+  | "CheckInDuoTone"
+  | "CheckInFill_1"
+  | "CheckInFill_2"
+  | "CheckInFill_3"
+  | "CheckInFill_4"
+  | "CheckInFill"
+  | "CheckInLine_1"
+  | "CheckInLine_2"
+  | "CheckInLine_3"
+  | "CheckInLine_4"
+  | "CheckInLine"
+  | "ConferenceDuoTone"
+  | "ConferenceFill"
+  | "ConferenceLine"
+  | "DishDuoTone"
+  | "DishFill"
+  | "DishLine"
+  | "DoorManDuoTone"
+  | "DoorManFill"
+  | "DoorManLine"
+  | "DryCleanDuoTone"
+  | "DryCleanFill"
+  | "DryCleanLine"
+  | "ElectricOutletDuoTone"
+  | "ElectricOutletFill"
+  | "ElectricOutletLine"
+  | "FlowerDuoTone"
+  | "FlowerFill"
+  | "FlowerLine"
+  | "FrontOfficeDuoTone"
+  | "FrontOfficeFill"
+  | "FrontOfficeLine"
+  | "GameDuoTone"
+  | "GameFill"
+  | "GameLine"
+  | "GymDuoTone"
+  | "GymFill"
+  | "GymLine"
+  | "LiftDuoTone"
+  | "LiftFill"
+  | "LiftLine"
+  | "LiquorDuoTone"
+  | "LiquorFill"
+  | "LiquorLine"
+  | "MedicalDuoTone"
+  | "MedicalFill"
+  | "MedicalLine"
+  | "ParkingDuoTone"
+  | "ParkingFill"
+  | "ParkingLine"
+  | "PetDuoTone"
+  | "PetFill"
+  | "PetLine"
+  | "ScissorDuoTone"
+  | "ScissorFill"
+  | "ScissorLine"
+  | "SecurityDuoTone"
+  | "SecurityFill"
+  | "SecurityLine"
+  | "ShieldCheckDuoTone"
+  | "ShieldCheckFill"
+  | "ShieldCheckLine"
+  | "ShopDuoTone"
+  | "ShopFill"
+  | "ShopLine"
+  | "SnowFlakeDuoTone"
+  | "SnowFlakeFill"
+  | "SnowFlakeLine"
+  | "SwimmingPoolDuoTone"
+  | "SwimmingPoolFill"
+  | "SwimmingPoolLine"
+  | "TrackDuoTone"
+  | "TrackFill"
+  | "TrackLine"
+  | "TreeFenceDuoTone"
+  | "TreeFenceFill"
+  | "TreeFenceLine";
 
 export enum BookingjiniIcons {
-  i24HourFrontDeskDualTone = "24-hour-front-desk_dual-tone",
-  i24HourFrontDeskFill = "24-hour-front-desk_fill",
-  i24HourFrontDeskLine = "24-hour-front-desk_line",
-  i24HourSecurityDualTone = "24-Hour-Security_dual-tone",
-  i24HourSecurityFill = "24-Hour-Security_fill",
-  i24HourSecurityLine = "24-Hour-Security_line",
+  AcDuoTone = "ac-duo-tone",
+  AcFill = "ac-fill",
+  AcLine = "ac-line",
+  AdjoiningRoomDuoTone = "adjoining-room-duo-tone",
+  AdjoiningRoomFill = "adjoining-room-fill",
+  AdjoiningRoomLine = "adjoining-room-line",
+  AtmDuoTone = "atm-duo-tone",
+  AtmFill = "atm-fill",
+  AtmLine = "atm-line",
+  BabyDuoTone = "baby-duo-tone",
+  BabyFill = "baby-fill",
+  BabyLine = "baby-line",
+  BasketDuoTone = "basket-duo-tone",
+  BasketFill = "basket-fill",
+  BasketLine = "basket-line",
+  BooksDuoTone = "books-duo-tone",
+  BooksFill = "books-fill",
+  BooksLine = "books-line",
+  BroomDuoTone = "broom-duo-tone",
+  BroomFill = "broom-fill",
+  BroomLine = "broom-line",
+  CabDuoTone = "cab-duo-tone",
+  CabFill = "cab-fill",
+  CabLine = "cab-line",
+  CallBellDuoTone = "call-bell-duo-tone",
+  CallBellFill_1 = "call-bell-fill-1",
+  CallBellFill = "call-bell-fill",
+  CardsDueTone = "cards-due-tone",
+  CardsDuoTone = "cards-duo-tone",
+  CardsLine = "cards-line",
+  CheckInDuoTone_1 = "check-in-duo-tone-1",
+  CheckInDuoTone_2 = "check-in-duo-tone-2",
+  CheckInDuoTone_3 = "check-in-duo-tone-3",
+  CheckInDuoTone_4 = "check-in-duo-tone-4",
+  CheckInDuoTone = "check-in-duo-tone",
+  CheckInFill_1 = "check-in-fill-1",
+  CheckInFill_2 = "check-in-fill-2",
+  CheckInFill_3 = "check-in-fill-3",
+  CheckInFill_4 = "check-in-fill-4",
+  CheckInFill = "check-in-fill",
+  CheckInLine_1 = "check-in-line-1",
+  CheckInLine_2 = "check-in-line-2",
+  CheckInLine_3 = "check-in-line-3",
+  CheckInLine_4 = "check-in-line-4",
+  CheckInLine = "check-in-line",
+  ConferenceDuoTone = "conference-duo-tone",
+  ConferenceFill = "conference-fill",
+  ConferenceLine = "conference-line",
+  DishDuoTone = "dish-duo-tone",
+  DishFill = "dish-fill",
+  DishLine = "dish-line",
+  DoorManDuoTone = "door-man-duo-tone",
+  DoorManFill = "door-man-fill",
+  DoorManLine = "door-man-line",
+  DryCleanDuoTone = "dry-clean-duo-tone",
+  DryCleanFill = "dry-clean-fill",
+  DryCleanLine = "dry-clean-line",
+  ElectricOutletDuoTone = "electric-outlet-duo-tone",
+  ElectricOutletFill = "electric-outlet-fill",
+  ElectricOutletLine = "electric-outlet-line",
+  FlowerDuoTone = "flower-duo-tone",
+  FlowerFill = "flower-fill",
+  FlowerLine = "flower-line",
+  FrontOfficeDuoTone = "front-office-duo-tone",
+  FrontOfficeFill = "front-office-fill",
+  FrontOfficeLine = "front-office-line",
+  GameDuoTone = "game-duo-tone",
+  GameFill = "game-fill",
+  GameLine = "game-line",
+  GymDuoTone = "gym-duo-tone",
+  GymFill = "gym-fill",
+  GymLine = "gym-line",
+  LiftDuoTone = "lift-duo-tone",
+  LiftFill = "lift-fill",
+  LiftLine = "lift-line",
+  LiquorDuoTone = "Liquor-duo-tone",
+  LiquorFill = "Liquor-fill",
+  LiquorLine = "Liquor-line",
+  MedicalDuoTone = "medical-duo-tone",
+  MedicalFill = "medical-fill",
+  MedicalLine = "medical-line",
+  ParkingDuoTone = "parking-duo-tone",
+  ParkingFill = "parking-fill",
+  ParkingLine = "parking-line",
+  PetDuoTone = "pet-duo-tone",
+  PetFill = "pet-fill",
+  PetLine = "pet-line",
+  ScissorDuoTone = "scissor-duo-tone",
+  ScissorFill = "scissor-fill",
+  ScissorLine = "scissor-line",
+  SecurityDuoTone = "security-duo-tone",
+  SecurityFill = "security-fill",
+  SecurityLine = "security-line",
+  ShieldCheckDuoTone = "shield-check-duo-tone",
+  ShieldCheckFill = "shield-check-fill",
+  ShieldCheckLine = "shield-check-line",
+  ShopDuoTone = "shop-duo-tone",
+  ShopFill = "shop-fill",
+  ShopLine = "shop-line",
+  SnowFlakeDuoTone = "snow-flake-duo-tone",
+  SnowFlakeFill = "snow-flake-fill",
+  SnowFlakeLine = "snow-flake-line",
+  SwimmingPoolDuoTone = "swimming-pool-duo-tone",
+  SwimmingPoolFill = "swimming-pool-fill",
+  SwimmingPoolLine = "swimming-pool-line",
+  TrackDuoTone = "track-duo-tone",
+  TrackFill = "track-fill",
+  TrackLine = "track-line",
+  TreeFenceDuoTone = "tree-fence-duo-tone",
+  TreeFenceFill = "tree-fence-fill",
+  TreeFenceLine = "tree-fence-line",
 }
 
 export const BOOKINGJINI_ICONS_CODEPOINTS: { [key in BookingjiniIcons]: string } = {
-  [BookingjiniIcons.i24HourFrontDeskDualTone]: "61697",
-  [BookingjiniIcons.i24HourFrontDeskFill]: "61698",
-  [BookingjiniIcons.i24HourFrontDeskLine]: "61699",
-  [BookingjiniIcons.i24HourSecurityDualTone]: "61700",
-  [BookingjiniIcons.i24HourSecurityFill]: "61701",
-  [BookingjiniIcons.i24HourSecurityLine]: "61702",
+  [BookingjiniIcons.AcDuoTone]: "61697",
+  [BookingjiniIcons.AcFill]: "61698",
+  [BookingjiniIcons.AcLine]: "61699",
+  [BookingjiniIcons.AdjoiningRoomDuoTone]: "61700",
+  [BookingjiniIcons.AdjoiningRoomFill]: "61701",
+  [BookingjiniIcons.AdjoiningRoomLine]: "61702",
+  [BookingjiniIcons.AtmDuoTone]: "61703",
+  [BookingjiniIcons.AtmFill]: "61704",
+  [BookingjiniIcons.AtmLine]: "61705",
+  [BookingjiniIcons.BabyDuoTone]: "61706",
+  [BookingjiniIcons.BabyFill]: "61707",
+  [BookingjiniIcons.BabyLine]: "61708",
+  [BookingjiniIcons.BasketDuoTone]: "61709",
+  [BookingjiniIcons.BasketFill]: "61710",
+  [BookingjiniIcons.BasketLine]: "61711",
+  [BookingjiniIcons.BooksDuoTone]: "61712",
+  [BookingjiniIcons.BooksFill]: "61713",
+  [BookingjiniIcons.BooksLine]: "61714",
+  [BookingjiniIcons.BroomDuoTone]: "61715",
+  [BookingjiniIcons.BroomFill]: "61716",
+  [BookingjiniIcons.BroomLine]: "61717",
+  [BookingjiniIcons.CabDuoTone]: "61718",
+  [BookingjiniIcons.CabFill]: "61719",
+  [BookingjiniIcons.CabLine]: "61720",
+  [BookingjiniIcons.CallBellDuoTone]: "61721",
+  [BookingjiniIcons.CallBellFill_1]: "61722",
+  [BookingjiniIcons.CallBellFill]: "61723",
+  [BookingjiniIcons.CardsDueTone]: "61724",
+  [BookingjiniIcons.CardsDuoTone]: "61725",
+  [BookingjiniIcons.CardsLine]: "61726",
+  [BookingjiniIcons.CheckInDuoTone_1]: "61727",
+  [BookingjiniIcons.CheckInDuoTone_2]: "61728",
+  [BookingjiniIcons.CheckInDuoTone_3]: "61729",
+  [BookingjiniIcons.CheckInDuoTone_4]: "61730",
+  [BookingjiniIcons.CheckInDuoTone]: "61731",
+  [BookingjiniIcons.CheckInFill_1]: "61732",
+  [BookingjiniIcons.CheckInFill_2]: "61733",
+  [BookingjiniIcons.CheckInFill_3]: "61734",
+  [BookingjiniIcons.CheckInFill_4]: "61735",
+  [BookingjiniIcons.CheckInFill]: "61736",
+  [BookingjiniIcons.CheckInLine_1]: "61737",
+  [BookingjiniIcons.CheckInLine_2]: "61738",
+  [BookingjiniIcons.CheckInLine_3]: "61739",
+  [BookingjiniIcons.CheckInLine_4]: "61740",
+  [BookingjiniIcons.CheckInLine]: "61741",
+  [BookingjiniIcons.ConferenceDuoTone]: "61742",
+  [BookingjiniIcons.ConferenceFill]: "61743",
+  [BookingjiniIcons.ConferenceLine]: "61744",
+  [BookingjiniIcons.DishDuoTone]: "61745",
+  [BookingjiniIcons.DishFill]: "61746",
+  [BookingjiniIcons.DishLine]: "61747",
+  [BookingjiniIcons.DoorManDuoTone]: "61748",
+  [BookingjiniIcons.DoorManFill]: "61749",
+  [BookingjiniIcons.DoorManLine]: "61750",
+  [BookingjiniIcons.DryCleanDuoTone]: "61751",
+  [BookingjiniIcons.DryCleanFill]: "61752",
+  [BookingjiniIcons.DryCleanLine]: "61753",
+  [BookingjiniIcons.ElectricOutletDuoTone]: "61754",
+  [BookingjiniIcons.ElectricOutletFill]: "61755",
+  [BookingjiniIcons.ElectricOutletLine]: "61756",
+  [BookingjiniIcons.FlowerDuoTone]: "61757",
+  [BookingjiniIcons.FlowerFill]: "61758",
+  [BookingjiniIcons.FlowerLine]: "61759",
+  [BookingjiniIcons.FrontOfficeDuoTone]: "61760",
+  [BookingjiniIcons.FrontOfficeFill]: "61761",
+  [BookingjiniIcons.FrontOfficeLine]: "61762",
+  [BookingjiniIcons.GameDuoTone]: "61763",
+  [BookingjiniIcons.GameFill]: "61764",
+  [BookingjiniIcons.GameLine]: "61765",
+  [BookingjiniIcons.GymDuoTone]: "61766",
+  [BookingjiniIcons.GymFill]: "61767",
+  [BookingjiniIcons.GymLine]: "61768",
+  [BookingjiniIcons.LiftDuoTone]: "61769",
+  [BookingjiniIcons.LiftFill]: "61770",
+  [BookingjiniIcons.LiftLine]: "61771",
+  [BookingjiniIcons.LiquorDuoTone]: "61772",
+  [BookingjiniIcons.LiquorFill]: "61773",
+  [BookingjiniIcons.LiquorLine]: "61774",
+  [BookingjiniIcons.MedicalDuoTone]: "61775",
+  [BookingjiniIcons.MedicalFill]: "61776",
+  [BookingjiniIcons.MedicalLine]: "61777",
+  [BookingjiniIcons.ParkingDuoTone]: "61778",
+  [BookingjiniIcons.ParkingFill]: "61779",
+  [BookingjiniIcons.ParkingLine]: "61780",
+  [BookingjiniIcons.PetDuoTone]: "61781",
+  [BookingjiniIcons.PetFill]: "61782",
+  [BookingjiniIcons.PetLine]: "61783",
+  [BookingjiniIcons.ScissorDuoTone]: "61784",
+  [BookingjiniIcons.ScissorFill]: "61785",
+  [BookingjiniIcons.ScissorLine]: "61786",
+  [BookingjiniIcons.SecurityDuoTone]: "61787",
+  [BookingjiniIcons.SecurityFill]: "61788",
+  [BookingjiniIcons.SecurityLine]: "61789",
+  [BookingjiniIcons.ShieldCheckDuoTone]: "61790",
+  [BookingjiniIcons.ShieldCheckFill]: "61791",
+  [BookingjiniIcons.ShieldCheckLine]: "61792",
+  [BookingjiniIcons.ShopDuoTone]: "61793",
+  [BookingjiniIcons.ShopFill]: "61794",
+  [BookingjiniIcons.ShopLine]: "61795",
+  [BookingjiniIcons.SnowFlakeDuoTone]: "61796",
+  [BookingjiniIcons.SnowFlakeFill]: "61797",
+  [BookingjiniIcons.SnowFlakeLine]: "61798",
+  [BookingjiniIcons.SwimmingPoolDuoTone]: "61799",
+  [BookingjiniIcons.SwimmingPoolFill]: "61800",
+  [BookingjiniIcons.SwimmingPoolLine]: "61801",
+  [BookingjiniIcons.TrackDuoTone]: "61802",
+  [BookingjiniIcons.TrackFill]: "61803",
+  [BookingjiniIcons.TrackLine]: "61804",
+  [BookingjiniIcons.TreeFenceDuoTone]: "61805",
+  [BookingjiniIcons.TreeFenceFill]: "61806",
+  [BookingjiniIcons.TreeFenceLine]: "61807",
 };
